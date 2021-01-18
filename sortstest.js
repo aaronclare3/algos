9f1dba61fb191e4a8514d55cd18ec86f99f44ca7
aaronclare3 <aaronclare88@gmail.com>
2021-01-17 21:42:02 -0800

    removed trash from sortstest.js

--- sortstest.js
+++ sortstest.js
@@ -131,30 +131,5 @@ console.log("insert", insert([5,3,6,1]))
 
 
 
-const printname = function(name, age){
-    return "My name is " + name + " and i am " + age + " years old"
-}
-
-console.log(printname("Aaron", 26))
-
-const fizzbuzz = () => {
-    for(let i = 1; i < 100; i++){
-        if(i % 5 == 0 && i % 3 == 0){
-            console.log("fizzbuzz");
-        }else if(i % 3 == 0){
-            console.log("fizz");
-        }else if(i % 5 == 0){
-            console.log("buzz");
-        }else{
-            console.log(i)
-        }
-    }
-}
-
-console.log(fizzbuzz());
-
-
-
-
 
 

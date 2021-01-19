4a37bf2253bd928838775a00a825c523ad7c384c
aaronclare3 <aaronclare88@gmail.com>
2021-01-18 21:09:44 -0800

    added meeting scheduler

--- mediumleetcode.js
+++ mediumleetcode.js
@@ -96,3 +96,66 @@ var swapNodes = function(head, k) {
     r1.val = temp;
     return head;
 }
+
+
+
+// MEETING SCHEDULER
+// given 2 peoples scheduled meetings and daily availability ranges, return the time frames that you could schedule a meeting of x duration that would work for both parties.
+
+// input:
+// schedule 1 meetings: [[9,10:30],[12,13],[16,18]]
+// person 1 hours: [9, 20]
+// schedule 2 meetings: [[10,11.5],[12.5,14.5],[14.5,15],[16,17]]
+// person 2 hours: [10, 18:30]
+// meeting duration: 0.5 (hours)
+
+// output: 
+// (available times to schedule the meeting): [[11.5,12],[15,16],[18,18.5]]
+
+const sched = (arr1, bounds1, arr2, bounds2, block) => {
+    let earliestTimeAvailable = Math.max(bounds1[0], bounds2[0]);
+    let latestTimeAvailable = Math.min(bounds1[1], bounds2[1]);
+    let combinedMeetings = [[earliestTimeAvailable]];
+    while(arr1.length && arr2.length){
+        if(arr1[0][0] < arr2[0][0]){
+            combinedMeetings.push(arr1.shift());
+        } else if(arr1[0][0] == arr2[0][0]){
+            if(arr1[0][1] <= arr2[0][1]){
+                combinedMeetings.push(arr1.shift())
+            }else{
+                combinedMeetings.push(arr2.shift());
+            }
+        }else {
+            combinedMeetings.push(arr2.shift());
+        }
+    } 
+    let combinedSchedules = combinedMeetings.concat(arr1).concat(arr2);
+    combinedSchedules.push([latestTimeAvailable]);    
+    // let combinedSchedules = [...combinedMeetings.concat(arr1).concat(arr2), [lastBound]
+    return checkFreeSpace(combinedSchedules, block);
+}
+
+const checkFreeSpace = (combinedMeetings, block) => {
+    let openSlots = [];
+    for(let i = 0; i < combinedMeetings.length-1; i++){
+        if(combinedMeetings[i].length == 1 && combinedMeetings[i+1][0] - combinedMeetings[i][0] >= block){
+            openSlots.push([combinedMeetings[i][0], combinedMeetings[i+1][0]])
+        }
+        else if(combinedMeetings[i+1][0] - combinedMeetings[i][1] >= block ){
+            openSlots.push([combinedMeetings[i][1], combinedMeetings[i+1][0]])
+        }
+    }
+    return openSlots;
+}
+
+
+let sched1 = [[9,10.5],[12,13],[16,18]];
+let sched2 = [[10,11.5], [12.5,14.5], [14.5,15], [16,17]];
+let sched1bound = [9,20];
+let sched2bound = [9.5,18.5];
+let meetingDurationHours = .5;
+
+console.log(sched(sched1, sched1bound, sched2, sched2bound, meetingDurationHours));
+
+
+

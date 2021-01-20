4cd605798f2627a589a4a98ee3461357f14a8328
aaronclare3 <aaronclare88@gmail.com>
2021-01-19 17:10:10 -0800

    added 2 sum

--- easyleetcode.js
+++ easyleetcode.js
@@ -743,3 +743,28 @@ const findNumbers = (nums) => {
 };
 
 
+// 167. Two Sum II - Input array is sorted
+// Given an array of integers that is already sorted in ascending order, find two numbers such that they add up to a specific target number.
+
+// The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2.
+
+// Note:
+
+// Your returned answers (both index1 and index2) are not zero-based.
+// You may assume that each input would have exactly one solution and you may not use the same element twice.
+
+
+const twoSum = (numbers, target) => {  
+    let p1 = 0;
+    let p2 = numbers.length-1;
+    while(numbers[p1] + numbers[p2] !== target){
+        if(numbers[p2] + numbers[p1] > target){
+            p2--;
+        }else{
+            p1++;
+        }
+    }
+    return [p1+1, p2+1];
+};
+
+

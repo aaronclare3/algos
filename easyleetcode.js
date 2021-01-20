560b2a1509641a2efc1943b16657e55492d1a905
aaronclare3 <aaronclare88@gmail.com>
2021-01-19 16:23:32 -0800

    added find numbers with even number digits

--- easyleetcode.js
+++ easyleetcode.js
@@ -728,5 +728,18 @@ console.log("count good triplets", countGoodTriplets([3,0,1,1,9,7], 7,2,3))
 
 
 
+// 1295. Find Numbers with Even Number of Digits
+// Input: nums = [12,345,2,6,7896]
+// Output: 2
+
+const findNumbers = (nums) => {
+    let count = 0;
+    for(let i = 0; i < nums.length; i++){
+        if(nums[i].toString().length % 2 == 0){
+            count++
+        }
+    }
+    return count;
+};
 
 

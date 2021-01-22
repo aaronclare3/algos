f6a646cbcdabf24826171d0934a438a72fe79d5c
aaronclare3 <aaronclare88@gmail.com>
2021-01-22 14:26:44 -0800

    added self-dividing numbers

--- easyleetcode.js
+++ easyleetcode.js
@@ -869,5 +869,34 @@ const repeatedNTimes = (A) => {
 };
 
 
+// 728. Self Dividing Numbers
+// A self-dividing number is a number that is divisible by every digit it contains. For example, 128 is a self-dividing number because 128 % 1 == 0, 128 % 2 == 0, and 128 % 8 == 0. Also, a self-dividing number is not allowed to contain the digit zero.
+
+// Given a lower and upper number bound, output a list of every possible self dividing number, including the bounds if possible.
+
+// Example 1:
+// Input: 
+// left = 1, right = 22
+// Output: [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 15, 22]
+
+
+const selfDividingNumbers = (left, right) => {
+    let count = [];
+    for(let i = left; i < right+1; i++){
+        let arr = i.toString().split('').map(nums => parseInt(nums));
+        let isDiv = true;
+        for(let j in arr){
+            if(i % arr[j] !== 0) {
+                 isDiv = false;
+            }
+        }
+        if(isDiv){
+            count.push(i)
+        }
+    }
+    return count;
+};
+
+console.log(selfDividingNumbers(1,22))
 
 

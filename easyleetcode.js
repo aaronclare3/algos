1810dbd8bb1bee9cb9a926d3f2dba2893e163200
aaronclare3 <aaronclare88@gmail.com>
2021-01-22 13:47:12 -0800

    N-Repeated Element in Size 2N Array

--- easyleetcode.js
+++ easyleetcode.js
@@ -846,5 +846,28 @@ const uniqueMorseRepresentations = (words) => {
 
 
 
+// 961. N-Repeated Element in Size 2N Array
+// In a array A of size 2N, there are N+1 unique elements, and exactly one of these elements is repeated N times.
+
+// Return the element repeated N times.
+
+// Example 1:
+// Input: [1,2,3,3]
+// Output: 3
+
+const repeatedNTimes = (A) => {
+    let n = A.length/2;
+    let obj = {}
+    for(let i = 0; i < A.length; i++){
+        let curr = A[i];
+        if(obj[curr]){
+            return curr;
+        }else{
+            obj[curr] = 1
+        }
+    }
+};
+
+
 
 

2cba91093a0bdf3dc40b0e50fc54bb37878e78dd
aaronclare3 <aaronclare88@gmail.com>
2021-01-25 08:29:37 -0800

    fixed insertion sort

--- sorts.js
+++ sorts.js
@@ -35,14 +35,13 @@ console.log("bubble sort", bubbleSort([5,4,3,2,1]));
 
 const insertionSort = arr => {
     for(let i = 1; i < arr.length; i++){
-        if(arr[i] < arr[i-1]){
-            while(arr[i] < arr[i-1]){
-                let temp = arr[i];
-                arr[i] = arr[i-1];
-                arr[i-1] = temp;
-                i--;
-            }
+        let j = i - 1;
+        let curr = arr[i];
+        while(arr[j] > curr && j >= 0){
+            arr[j+1] = arr[j];
+            j--;
         }
+        arr[j+1] = curr;
     }
     return arr;
 }

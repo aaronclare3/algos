e9c64cdf7ae5ffce3e81fad7a6ccad193983c2eb
aaronclare3 <aaronclare88@gmail.com>
2021-02-03 15:50:22 -0800

    added findTheDistanceValue

--- easyleetcode.js
+++ easyleetcode.js
@@ -947,3 +947,20 @@ const findSpecialInteger = (arr) => {
     }
   }
 };
+
+// Given two integer arrays arr1 and arr2, and the integer d, return the distance value between the two arrays.
+
+// The distance value is defined as the number of elements arr1[i] such that there is not any element arr2[j] where |arr1[i]-arr2[j]| <= d.
+
+const findTheDistanceValue = (arr1, arr2, d) => {
+  let count = 0;
+  for (let i = 0; i < arr1.length; i++) {
+    for (let j = 0; j < arr2.length; j++) {
+      if (Math.abs(arr1[i] - arr2[j]) <= d) {
+        count++;
+        break;
+      }
+    }
+  }
+  return arr1.length - count;
+};

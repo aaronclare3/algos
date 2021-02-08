65444c74de449d3c379e9dea8c80814cd90ad779
aaronclare3 <aaronclare88@gmail.com>
2021-02-07 23:58:11 -0800

    added Can Place Flowers

--- easyleetcode.js
+++ easyleetcode.js
@@ -994,5 +994,31 @@ const firstLetter = (str) => {
   else if (first === first.toLowerCase()) return "Lower";
 };
 
-console.log(firstLetter("Fizz"));
-console.log("@".toUpperCase() === "@");
+const removeWhiteSpace = (str) => {
+  const whitespace = " ";
+  let ans = "";
+  for (let i = 0; i < str.length - 1; i++) {
+    if (str[i] == whitespace) {
+      i++;
+    }
+    ans += str[i];
+  }
+  return ans;
+};
+
+console.log(removeWhiteSpace("hello world it is me h"));
+
+// 605. Can Place Flowers
+// You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted in adjacent plots.
+// Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, and an integer n, return if n new flowers can be planted in the flowerbed without violating the no-adjacent-flowers rule.
+
+const canPlaceFlowers = (flowerbed, n) => {
+  if (n === 0) return true;
+  for (let i = 0; i < flowerbed.length; i++) {
+    if (flowerbed[i] != 1 && flowerbed[i + 1] !== 1 && flowerbed[i - 1] !== 1) {
+      flowerbed[i] = 1;
+      n--;
+    }
+  }
+  return n <= 0;
+};

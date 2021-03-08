4c52646b35dd0394605452e1a242ae31cea8ebc7
aaronclare3 <aaronclare88@gmail.com>
2021-03-07 19:12:58 -0800

    added a couple interview prep questions

--- codesignal.js
+++ codesignal.js
@@ -169,3 +169,52 @@ function rotateImage(a) {
   }
   return a;
 }
+
+// Given an array a that contains only numbers in the range from 1 to a.length, find the first duplicate number for which the second occurrence has the minimal index. In other words, if there are more than 1 duplicated numbers, return the number for which the second occurrence has a smaller index than the second occurrence of the other number does. If there are no such elements, return -1.
+// Example
+// For a = [2, 1, 3, 5, 3, 2], the output should be firstDuplicate(a) = 3.
+// There are 2 duplicates: numbers 2 and 3. The second occurrence of 3 has a smaller index than the second occurrence of 2 does, so the answer is 3.
+// For a = [2, 2], the output should be firstDuplicate(a) = 2;
+// For a = [2, 4, 3, 5, 1], the output should be firstDuplicate(a) = -1.
+
+function firstDuplicate(a) {
+  let dups = {};
+  for (let i = 0; i < a.length; i++) {
+    if (dups[a[i]]) {
+      return a[i];
+    } else {
+      dups[a[i]] = 1;
+    }
+  }
+  return -1;
+}
+
+// Given a string s consisting of small English letters, find and return the first instance of a non-repeating character in it. If there is no such character, return '_'.
+// Example
+// For s = "abacabad", the output should be
+// firstNotRepeatingCharacter(s) = 'c'.
+// There are 2 non-repeating characters in the string: 'c' and 'd'. Return c since it appears in the string first.
+// For s = "abacabaabacaba", the output should be
+// firstNotRepeatingCharacter(s) = '_'.
+// There are no characters in this string that do not repeat.
+
+function firstNotRepeatingCharacter(s) {
+  let noRepeat = {};
+  for (let i = 0; i < s.length; i++) {
+    let char = s[i];
+    if (noRepeat[char]) {
+      noRepeat[char].count++;
+    } else {
+      noRepeat[char] = { idx: i, count: 1 };
+    }
+  }
+  let min = null;
+  for (let key in noRepeat) {
+    if (noRepeat[key].count === 1) {
+      if (min === null || noRepeat[key].idx < min) {
+        min = noRepeat[key].idx;
+      }
+    }
+  }
+  return min === null ? "_" : s[min];
+}

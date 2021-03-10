6641ed6107299a0199f86c04e483fbe72697bb45
aaronclare3 <aaronclare88@gmail.com>
2021-03-10 01:01:26 -0800

    added arrays are similar

--- codesignal.js
+++ codesignal.js
@@ -245,3 +245,27 @@ const reverseInParentheses = (str) => {
   }
   return str;
 };
+
+// Two arrays are called similar if one can be obtained from another by swapping at most one pair of elements in one of the arrays.
+// Given two arrays a and b, check whether they are similar.
+// Example
+// For a = [1, 2, 3] and b = [1, 2, 3], the output should be
+// areSimilar(a, b) = true.
+// The arrays are equal, no need to swap any elements.
+// For a = [1, 2, 3] and b = [2, 1, 3], the output should be
+// areSimilar(a, b) = true.
+
+const areSimilar = (a, b) => {
+  let count = 0;
+  let index1 = null;
+  let index2 = null;
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] != b[i]) {
+      count++;
+      index1 == null ? (index1 = i) : (index2 = i);
+    }
+  }
+  if (count > 2 || a[index1] != b[index2] || a[index2] != b[index1])
+    return false;
+  return true;
+};

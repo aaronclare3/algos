c533317689dc0a128e5cb9c7a31fc9102f035f78
aaronclare3 <aaronclare88@gmail.com>
2021-03-14 20:54:17 -0700

    added array change

--- codesignal.js
+++ codesignal.js
@@ -269,3 +269,19 @@ const areSimilar = (a, b) => {
     return false;
   return true;
 };
+
+//You are given an array of integers. On each move you are allowed to increase exactly one of its element by one. Find the minimal number of moves required to obtain a strictly increasing sequence from the input.
+// Example
+// For inputArray = [1, 1, 1], the output should be
+// arrayChange(inputArray) = 3.
+
+const arrayChange = (arr) => {
+  let count = 0;
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] >= arr[i + 1]) {
+      count += Math.abs(arr[i] - arr[i + 1] + 1);
+      arr[i + 1] = arr[i] + 1;
+    }
+  }
+  return count;
+};

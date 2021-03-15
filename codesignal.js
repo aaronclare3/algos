69853ca52c04c98368193151c67e7bf53e6e3bbb
aaronclare3 <aaronclare88@gmail.com>
2021-03-14 20:54:31 -0700

    added strongest arms

--- codesignal.js
+++ codesignal.js
@@ -285,3 +285,17 @@ const arrayChange = (arr) => {
   }
   return count;
 };
+
+// Call two arms equally strong if the heaviest weights they each are able to lift are equal.
+// Call two people equally strong if their strongest arms are equally strong (the strongest arm can be both the right and the left), and so are their weakest arms.
+// Given your and your friend's arms' lifting capabilities find out if you two are equally strong.
+// Example
+// For yourLeft = 10, yourRight = 15, friendsLeft = 15, and friendsRight = 10, the output should be
+// areEquallyStrong(yourLeft, yourRight, friendsLeft, friendsRight) = true;
+
+const areEquallyStrong = (yourLeft, yourRight, friendsLeft, friendsRight) => {
+  return (
+    Math.max(yourLeft, yourRight) === Math.max(friendsRight, friendsLeft) &&
+    Math.min(friendsLeft, friendsRight) === Math.min(yourRight, yourLeft)
+  );
+};

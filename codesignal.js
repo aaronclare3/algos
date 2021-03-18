ee7161ebd9420eac20c0d2c6ce16dffc9136236a
aaronclare3 <aaronclare88@gmail.com>
2021-03-17 22:45:27 -0700

    added circle of numbers

--- codesignal.js
+++ codesignal.js
@@ -417,3 +417,15 @@ function chessBoardCellColor(cell1, cell2) {
   }
   return celly1 === celly2;
 }
+
+// Consider integer numbers from 0 to n - 1 written down along the circle in such a way that the distance between any two neighboring numbers is equal (note that 0 and n - 1 are neighboring, too).
+// Given n and firstNumber, find the number which is written in the radially opposite position to firstNumber.
+// Example
+// For n = 10 and firstNumber = 2, the output should be
+// circleOfNumbers(n, firstNumber) = 7.
+
+function circleOfNumbers(n, firstNumber) {
+  let x = n / 2 + firstNumber;
+  // if we surpass the number of elements (n), we return the remainder of x / n
+  return x >= n ? x % n : x;
+}

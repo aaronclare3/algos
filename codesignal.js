57ec42c7687a370fe37fb7160501da226f8294e6
aaronclare3 <aaronclare88@gmail.com>
2021-03-23 19:11:19 -0700

    digit degree

--- codesignal.js
+++ codesignal.js
@@ -546,3 +546,29 @@ const arrayMaxConsecutiveSum = (a, k) => {
   }
   return max;
 };
+
+// Let's define digit degree of some positive integer as the number of times we need to replace this number with the sum of its digits until we get to a one digit number.
+// Given an integer, find its digit degree.
+// Example
+// For n = 5, the output should be
+// digitDegree(n) = 0;
+// For n = 100, the output should be
+// digitDegree(n) = 1.
+// 1 + 0 + 0 = 1.
+// For n = 91, the output should be
+// digitDegree(n) = 2.
+// 9 + 1 = 10 -> 1 + 0 = 1.
+
+const digitDegree = (n) => {
+  let count = 0;
+  if (n < 10 && n > -1) return 0;
+  while (n >= 10 || n < 0) {
+    let x = n.toString();
+    count++;
+    n = 0;
+    for (let i = 0; i < x.length; i++) {
+      n += Number(x[i]);
+    }
+  }
+  return count;
+};

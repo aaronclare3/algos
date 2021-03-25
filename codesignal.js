0d95363e544b71b20cc1caeac55ebb45250d7ef5
aaronclare3 <aaronclare88@gmail.com>
2021-03-25 15:00:56 -0700

    beautiful string

--- codesignal.js
+++ codesignal.js
@@ -592,3 +592,46 @@ const bishopAndPawn = (bishop, pawn) => {
     ? true
     : false;
 };
+
+// A string is said to be beautiful if each letter in the string appears at most as many times as the previous letter in the alphabet within the string; ie: b occurs no more times than a; c occurs no more times than b; etc. Note that letter a has no previous letter.
+// Given a string, check whether it is beautiful.
+// Example
+// For inputString = "bbbaacdafe", the output should be isBeautifulString(inputString) = true.
+// This string contains 3 as, 3 bs, 1 c, 1 d, 1 e, and 1 f (and 0 of every other letter), so since there aren't any letters that appear more frequently than the previous letter, this string qualifies as beautiful.
+// For inputString = "aabbb", the output should be isBeautifulString(inputString) = false.
+// Since there are more bs than as, this string is not beautiful.
+// For inputString = "bbc", the output should be isBeautifulString(inputString) = false.
+// Although there are more bs than cs, this string is not beautiful because there are no as, so therefore there are more bs than as.
+
+const isBeautifulString = (a) => {
+  let bucket = [];
+  let x = a.split("").sort().join("");
+  for (let i = 0; i < x.length; i++) {
+    let f = i;
+    let chunk = "";
+    if (x[i] === x[f]) {
+      while (x[i] === x[f]) {
+        chunk += x[f];
+        f++;
+      }
+      i = f - 1;
+    } else {
+      chunk = a[i];
+    }
+    bucket.push(chunk);
+  }
+  return checkBucket(bucket);
+};
+
+const checkBucket = (arr) => {
+  if (!arr[0].startsWith("a")) return false;
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (
+      arr[i].length < arr[i + 1].length ||
+      arr[i + 1].charCodeAt(0) - arr[i].charCodeAt(0) > 1
+    ) {
+      return false;
+    }
+  }
+  return true;
+};

d8b6b3b748acd66a906d68e338b80c811d2cd26d
aaronclare3 <aaronclare88@gmail.com>
2021-03-31 20:59:18 -0700

    added line encoding

--- codesignal.js
+++ codesignal.js
@@ -662,3 +662,28 @@ const buildPalindrome = (st) => {
 const isPalindrome = (string) => {
   return string == Array.from(string).reverse().join("");
 };
+
+// Given a string, return its encoding defined as follows:
+// First, the string is divided into the least possible number of disjoint substrings consisting of identical characters
+// for example, "aabbbc" is divided into ["aa", "bbb", "c"]
+// Next, each substring with length greater than one is replaced with a concatenation of its length and the repeating character
+// for example, substring "bbb" is replaced by "3b"
+// Finally, all the new strings are concatenated together in the same order and a new string is returned.
+// Example
+// For s = "aabbbc", the output should be
+// lineEncoding(s) = "2a3bc".
+
+function lineEncoding(s) {
+  let count = 1;
+  let newS = "";
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === s[i + 1]) {
+      count++;
+    } else {
+      if (count === 1) newS = newS + s[i];
+      else newS = newS + count + s[i];
+      count = 1;
+    }
+  }
+  return newS;
+}

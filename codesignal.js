78b610e852a1c61008515f14354ec5db72bf29bf
aaronclare3 <aaronclare88@gmail.com>
2021-04-03 20:58:13 -0700

    added sudoku

--- codesignal.js
+++ codesignal.js
@@ -959,3 +959,75 @@ function spiralNumbers(n) {
   }
   return ans;
 }
+
+// Sudoku is a number-placement puzzle. The objective is to fill a 9 × 9 grid with digits so that each column, each row, and each of the nine 3 × 3 sub-grids that compose the grid contains all of the digits from 1 to 9.
+// This algorithm should check if the given grid of numbers represents a correct solution to Sudoku.
+// Example
+
+// For
+// grid = [[1, 3, 2, 5, 4, 6, 9, 8, 7],
+//         [4, 6, 5, 8, 7, 9, 3, 2, 1],
+//         [7, 9, 8, 2, 1, 3, 6, 5, 4],
+//         [9, 2, 1, 4, 3, 5, 8, 7, 6],
+//         [3, 5, 4, 7, 6, 8, 2, 1, 9],
+//         [6, 8, 7, 1, 9, 2, 5, 4, 3],
+//         [5, 7, 6, 9, 8, 1, 4, 3, 2],
+//         [2, 4, 3, 6, 5, 7, 1, 9, 8],
+//         [8, 1, 9, 3, 2, 4, 7, 6, 5]]
+// the output should be
+// sudoku(grid) = true;
+
+// For
+// grid = [[1, 3, 2, 5, 4, 6, 9, 2, 7],
+//         [4, 6, 5, 8, 7, 9, 3, 8, 1],
+//         [7, 9, 8, 2, 1, 3, 6, 5, 4],
+//         [9, 2, 1, 4, 3, 5, 8, 7, 6],
+//         [3, 5, 4, 7, 6, 8, 2, 1, 9],
+//         [6, 8, 7, 1, 9, 2, 5, 4, 3],
+//         [5, 7, 6, 9, 8, 1, 4, 3, 2],
+//         [2, 4, 3, 6, 5, 7, 1, 9, 8],
+//         [8, 1, 9, 3, 2, 4, 7, 6, 5]]
+// the output should be
+// sudoku(grid) = false.
+
+function sudoku(grid) {
+  if (!checkRows(grid) || !checkCols(grid) || !checkBlocks(grid)) return false;
+  return true;
+}
+
+const checkRows = (grid) => {
+  for (let r = 0; r < 9; r++) {
+    let mark = {};
+    for (let c = 0; c < 9; c++) {
+      if (mark[grid[c][r]]) return false;
+      mark[grid[c][r]] = 1;
+    }
+  }
+  return true;
+};
+
+const checkCols = (grid) => {
+  for (let r = 0; r < 9; r++) {
+    let mark = {};
+    for (let c = 0; c < 9; c++) {
+      if (mark[grid[r][c]]) return false;
+      mark[grid[r][c]] = 1;
+    }
+  }
+  return true;
+};
+
+const checkBlocks = (grid) => {
+  for (let r = 0; r < 9; r += 3) {
+    for (let c = 0; c < 9; c += 3) {
+      let mark = {};
+      for (let row = r; row < 3 + r; row++) {
+        for (let col = c; col < 3 + c; col++) {
+          if (mark[grid[row][col]]) return false;
+          mark[grid[row][col]] = 1;
+        }
+      }
+    }
+  }
+  return true;
+};

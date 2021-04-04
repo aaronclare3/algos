c1eb98146cf9fe9bdb159040bfbebf542b2ea179
aaronclare3 <aaronclare88@gmail.com>
2021-04-03 20:47:37 -0700

    sum up numbers

--- codesignal.js
+++ codesignal.js
@@ -783,3 +783,30 @@ function validTime(time) {
     return true;
   return false;
 }
+
+// CodeMaster has just returned from shopping. He scanned the check of the items he bought and gave the resulting string to Ratiorg to figure out the total number of purchased items. Since Ratiorg is a bot he is definitely going to automate it, so he needs a program that sums up all the numbers which appear in the given input.
+// Help Ratiorg by writing a function that returns the sum of numbers that appear in the given inputString.
+// Example
+// For inputString = "2 apples, 12 oranges", the output should be
+// sumUpNumbers(inputString) = 14.
+
+function sumUpNumbers(input) {
+  let ans = "";
+  let sum = 0;
+  let regex = /^\d+/g;
+  for (let i = 0; i < input.length; i++) {
+    if (input[i].match(regex)) {
+      ans += input[i];
+    } else {
+      if (ans.length >= 1) {
+        console.log(Number(ans));
+        sum += Number(ans);
+        ans = "";
+      }
+    }
+    if (i === input.length - 1 && ans.length >= 1) {
+      sum += Number(ans);
+    }
+  }
+  return sum;
+}

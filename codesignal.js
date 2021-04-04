fab63e6e4e6daa5e8658840c832a5e6225639657
aaronclare3 <aaronclare88@gmail.com>
2021-04-03 20:49:10 -0700

    added differentSquares

--- codesignal.js
+++ codesignal.js
@@ -810,3 +810,33 @@ function sumUpNumbers(input) {
   }
   return sum;
 }
+
+// Given a rectangular matrix containing only digits, calculate the number of different 2 × 2 squares in it.
+// Example
+// For
+// matrix = [[1, 2, 1],
+//           [2, 2, 2],
+//           [2, 2, 2],
+//           [1, 2, 3],
+//           [2, 2, 1]]
+// the output should be
+// differentSquares(matrix) = 6.
+
+function differentSquares(matrix) {
+  let obj = {};
+  let count = 0;
+  for (let i = 0; i < matrix.length - 1; i++) {
+    for (let j = 0; j < matrix[i].length - 1; j++) {
+      let x =
+        String(matrix[i][j]) +
+        String(matrix[i][j + 1]) +
+        String(matrix[i + 1][j]) +
+        String(matrix[i + 1][j + 1]);
+      if (!obj[x]) {
+        obj[x] = 1;
+        count++;
+      }
+    }
+  }
+  return count;
+}

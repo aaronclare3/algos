da2f134bba1dd4f083c389c917bdb3df9fbfd1b1
aaronclare3 <aaronclare88@gmail.com>
2021-04-05 22:59:20 -0700

    added isInfiniteProcess

--- codesignal.js
+++ codesignal.js
@@ -1108,3 +1108,19 @@ function phoneCall(min1, min2_10, min11, s) {
   }
   return min;
 }
+
+// Given integers a and b, determine whether the following pseudocode results in an infinite loop
+// while a is not equal to b do
+// increase a by 1
+// decrease b by 1
+// Assume that the program is executed on a virtual machine which can store arbitrary long numbers and execute forever.
+// Example
+// For a = 2 and b = 6, the output should be
+// isInfiniteProcess(a, b) = false;
+// For a = 2 and b = 3, the output should be
+// isInfiniteProcess(a, b) = true.
+
+function isInfiniteProcess(a, b) {
+  if (a <= b && a % 2 === b % 2) return false;
+  return true;
+}
